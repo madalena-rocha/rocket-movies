e9bf15c619353696aa1456729081ff55c1705f1c
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-28 20:46:19 -0300

    :zap: finish page SignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,8 +1,47 @@
-import { Container } from "./styles";
+import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Background } from "./styles";
 
 export function SignUp() {
   return (
     <Container>
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+
+        <h2>Crie sua conta</h2>
+
+        <Input 
+          placeholder="Nome" 
+          type="text" 
+          icon={FiUser} 
+        />
+
+        <Input 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail} 
+        />
+
+        <Input 
+          placeholder="Senha" 
+          type="password" 
+          icon={FiLock} 
+        />
+
+        <Button title="Cadastrar" />
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para o login
+        </Link>
+      </Form>
+
+      <Background />
     </Container>
   );
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,3 +1,48 @@
 import styled from "styled-components";
+import backgroundImg from "../../assets/background.png";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Form = styled.form`
+  padding: 0 161px 0 136px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  > h1 {
+    font-size: 48px;
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  > h2 {
+    font-size: 24px;
+    margin: 48px 0;
+  }
+
+  > p {
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_100};
+  }
+
+  > a {
+    margin-top: 42px;
+    color: ${({ theme }) => theme.COLORS.PINK};
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${backgroundImg}) no-repeat center center;
+  background-size: cover;
+`;

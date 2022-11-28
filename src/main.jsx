3cf0a5b8b1592fd142979c5fd1e5f87ba3a36b92
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-28 15:51:41 -0300

    :zap: create page New

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
-import { SignIn } from './pages/SignIn';
+import { New } from './pages/New';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/New/index.jsx
@@ -0,0 +1,8 @@
+import { Container } from "./styles";
+
+export function New() {
+  return (
+    <Container>
+    </Container>
+  );
+}

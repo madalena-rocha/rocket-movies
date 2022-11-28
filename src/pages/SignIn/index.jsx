2718baddd534e9bcc42625bf1d71e45d401208cb
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-28 12:56:54 -0300

    :sparkles: add component Input

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles";
 
+import { Input } from '../../components/Input';
 import { Button } from "../../components/Button";
 
 export function SignIn() {
   return (
     <Container>
+      <Input placeholder="E-mail" />
+      <Input placeholder="Senha" />
+
       <Button title="Entrar" />
     </Container>
   );

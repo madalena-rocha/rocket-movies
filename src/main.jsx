ff0c258c182ab2c2857d4e6de7b66d823754c371
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-28 20:17:39 -0300

    :zap: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
 
-import { Details } from './pages/Details';
+import { Routes } from './Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )

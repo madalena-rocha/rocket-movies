df9dd53b93ef87d485bfd795065ce2eeaf3830c4
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-28 20:58:56 -0300

    :zap: finish page Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,8 +1,64 @@
-import { Container } from "./styles";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Avatar } from "./styles";
 
 export function Profile() {
   return (
     <Container>
+      <header>
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar
+        </Link>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img
+            src="https://github.com/madalena-rocha.png"
+            alt="Foto do usuário"
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input 
+              id="avatar" 
+              type="file" 
+            />
+          </label>
+        </Avatar>
+
+        <Input 
+          placeholder="Nome" 
+          type="text" 
+          icon={FiUser} 
+        />
+
+        <Input 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail} 
+        />
+
+        <Input 
+          placeholder="Senha atual" 
+          type="password" 
+          icon={FiLock} 
+        />
+
+        <Input 
+          placeholder="Nova Senha" 
+          type="password" 
+          icon={FiLock} 
+        />
+
+        <Button title="Salvar" disabled />
+      </Form>
     </Container>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,75 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+
+  > header {
+    width: 100%;
+    height: 144px;
+
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+
+    display: flex;
+    align-items: center;
+
+    padding: 0 144px;
+
+    a {
+      color: ${({ theme }) => theme.COLORS.PINK};
+      
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  max-width: 340px;
+  margin: 30px auto;
+
+  > div:nth-child(4) {
+    margin-top: 16px;
+  }
+`;
+
+export const Avatar = styled.div`
+  position: relative;
+  margin: -124px auto 64px;
+
+  width: 186px;
+  height: 186px;
+
+  > img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+  > label {
+    width: 48px;
+    height: 48px;
+
+    background-color: ${({ theme }) => theme.COLORS.PINK};
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    position: absolute;
+    bottom: 7px;
+    right: 7px;
+
+    cursor: pointer;
+
+    input {
+      display: none;
+    }
+    
+    svg {
+      width: 20px;
+      height: 20px;
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    }
+  }
+`;

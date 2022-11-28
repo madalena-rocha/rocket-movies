9ab3f0d5a064a8e4285b6fe6cd6df213c49a984d
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-28 20:44:24 -0300

    :zap: finish page SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,15 +1,40 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-import { Input } from '../../components/Input';
+import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+
+import { Container, Form, Background } from "./styles";
 
 export function SignIn() {
   return (
     <Container>
-      <Input placeholder="E-mail" />
-      <Input placeholder="Senha" />
+      <Form>
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
 
-      <Button title="Entrar" />
+        <h2>Faça seu login</h2>
+
+        <Input 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail} 
+        />
+
+        <Input 
+          placeholder="Senha" 
+          type="password" 
+          icon={FiLock} 
+        />
+
+        <Button title="Entrar" />
+
+        <Link to="/register">
+          Criar conta
+        </Link>
+      </Form>
+
+      <Background />
     </Container>
   );
 }

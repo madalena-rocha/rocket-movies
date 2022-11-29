0ba79cfe3a372c00f273fe498b112e95327bf83a
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-28 21:11:39 -0300

    :zap: finish page Home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,60 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 116px auto;
+  grid-template-areas:
+    "header"
+    "content";
+  
+  > main {
+    grid-area: content;
+
+    max-width: 1137px;
+    margin: 48px auto;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      margin-bottom: 38px;
+
+      h1 {
+        font-weight: 400;
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  max-height: calc(100vh - 297px);
+  overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.COLORS.PINK};
+    border-radius: 8px;
+  }
+`;
+
+export const NewMovie = styled(Link)`
+  background-color: ${({ theme }) => theme.COLORS.PINK};
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  border: none;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  
+  padding: 13.5px 32px;
+  border-radius: 8px;
+`;

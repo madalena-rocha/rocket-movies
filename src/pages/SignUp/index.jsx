6a2e761124a96c1c5e5a8531091dafa33d25e061
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-12-09 16:38:43 -0300

    :sparkles: add user registration

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { api } from "../../services/api";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -7,6 +10,31 @@ import { Button } from "../../components/Button";
 import { Container, Form, Background } from "./styles";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const navigate = useNavigate();
+
+	function handleSignUp() {
+		if (!name || !email || !password) {
+			return alert("Preencha todos os campos!");
+		}
+
+		api.post("/users", { name, email, password })
+			.then(() => {
+				alert("Usuário cadastrado com sucesso!");
+				navigate("/");
+			})
+			.catch(error => {
+				if(error.response){
+					alert(error.response.data.message);
+				} else {
+					alert("Não foi possível cadastrar");
+				}
+			})
+	}
+
   return (
     <Container>
       <Form>
@@ -19,21 +47,24 @@ export function SignUp() {
           placeholder="Nome" 
           type="text" 
           icon={FiUser} 
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail" 
           type="text" 
           icon={FiMail} 
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha" 
           type="password" 
           icon={FiLock} 
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link to="/">
           <FiArrowLeft />

7c5c8f3dc441ca5fc44af16115951b5b612eb63f
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-12-10 13:37:36 -0300

    :sparkles: add movie registration

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { FiArrowLeft } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
+import { useNavigate } from 'react-router-dom';
+
 import { Textarea } from "../../components/Textarea";
 import { MovieItem } from "../../components/MovieItem";
 import { Section } from "../../components/Section";
@@ -9,11 +11,19 @@ import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 
+import { api } from '../../services/api';
+
 import { Container, Form } from "./styles";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
+	const [description, setDescription] = useState("");
+
   const [tags, setTags] = useState([]);
 	const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
 
   function handleAddTag() {
 		setTags(prevState => [...prevState, newTag])
@@ -23,6 +33,42 @@ export function New() {
   function handleRemoveTag(deleted) {
 		setTags(prevState => prevState.filter(tag => tag !== deleted));
 	}
+
+  async function handleNewMovie() {
+    if (!title) {
+			return alert("Digite o título do filme");
+		}
+    
+    const isRatingValid = rating >= 0 && rating <= 5;
+
+    if (!isRatingValid) {
+      return alert("A nota do filme deve ser entre 0 e 5");
+    }
+    
+		if (newTag) {
+			return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.");
+		}
+
+		await api.post("/notes", {
+			title,
+			description,
+      rating,
+			tags
+		});
+
+		alert("Filme adicionado com sucesso!");
+		navigate("/");
+	}
+
+  function handleDiscardMovie() {
+    const userConfirmation = confirm(
+      "Todas as alterações serão perdidas... Tem certeza que deseja descartar as alterações?"
+    );
+
+    if (userConfirmation) {
+      navigate("/");
+    }
+  }
 
   return (
     <Container>
@@ -40,11 +86,25 @@ export function New() {
           </header>
 
           <div>
-            <Input placeholder="Título" />
-            <Input placeholder="Sua nota (de 0 a 5)" />
+            <Input 
+              placeholder="Título" 
+              onChange={e => setTitle(e.target.value)}
+            />
+
+            <Input 
+              placeholder="Sua nota (de 0 a 5)" 
+              type="number"
+              min="0"
+              max="5"
+              value={rating}
+              onChange={e => setRating(e.target.value)}
+            />
           </div>
 
-          <Textarea placeholder="Observações" />
+          <Textarea 
+						placeholder="Observações"
+						onChange={e => setDescription(e.target.value)} 
+					/>
 
           <Section title="Marcadores">
             <div className="tags">
@@ -69,8 +129,15 @@ export function New() {
           </Section>
 
           <div>
-            <Button title="Excluir filme" />
-            <Button title="Salvar alterações" />
+            <Button 
+              title="Excluir filme" 
+              onClick={handleDiscardMovie}
+            />
+
+            <Button 
+              title="Salvar alterações" 
+              onClick={handleNewMovie}
+            />
           </div>
         </Form>
       </main>
